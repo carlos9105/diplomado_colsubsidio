ee20584697f71dbe0647807e13b55091f04956d7
carlos9105 <113561657+carlos9105@users.noreply.github.com>
2022-10-02 23:05:41 -0500

    actualizacion

---
+++ clase14/index14.js
@@ -0,0 +1,16 @@
+for (let a=0; a<5; a++){
+    square=""
+        for (let b=0; b<5; b++){
+            square=square+"X"
+    }
+    console.log(square);
+}
+
+/*let a=4
+let b=5
+
+if (a===b && typeof a === typeof b){
+    console.log("Hola1");
+} else {
+    console.log("NOP")
+}*/
---
+++ proyecto_semana_3/jsindex3.js
@@ -0,0 +1,171 @@
+function funcionalerta() {
+
+    var x = document.getElementById("alerta01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostarnavbar() {
+
+    var x = document.getElementById("navbar01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostarcarrusel() {
+
+    var x = document.getElementById("carrusel01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostarcards() {
+
+    var x = document.getElementById("cards01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostartabla() {
+
+    var x = document.getElementById("tabla01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostarlista() {
+
+    var x = document.getElementById("lista01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostarformulario() {
+
+    var x = document.getElementById("formulario01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostarfooter() {
+
+    var x = document.getElementById("footer01");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function mostartextos() {
+    var y = document.getElementById("texto01").innerHTML = "La Guajira";
+    var y = document.getElementById("texto02").innerHTML = "Planes y precios";
+    var y = document.getElementById("texto03").innerHTML = "¡Reserva ahora!";
+    var y = document.getElementById("texto04").innerHTML = "• LA GUAJIRA •";
+    var y = document.getElementById("texto04").style.color = "#E36E4C";
+    var y = document.getElementById("texto05").innerHTML = "Destino reconocido por sus hermosos paisajes, su gente, y su cultura";
+    var y = document.getElementById("texto05").style.color = "#E36E4C";
+    var y = document.getElementById("texto06").innerHTML = "Ubicado en la punta más norte de Colombia, este gran atractivo natural ofrece a sus visitantes la posibilidad de vivir las mejores experiencias en una combinación entre la tierra árida desértica y el mar. Te compartimos 6 razones por las cuales visitar este maravilloso destino:";
+    var y = document.getElementById("texto07").innerHTML = "CABO DE LA VELA";
+    var y = document.getElementById("texto08").innerHTML = "Uno de los atractivos más reconocidos en la Guajira, sus hermosas aguas color turquesa hacen de este mar el perfecto escenario para disfrutar de un hermoso atardecer rodeado de la cultura wayuu.";
+    var y = document.getElementById("texto09").innerHTML = "CULTURA WAYUU";
+    var y = document.getElementById("texto10").innerHTML = "La cultura indígena Wayuu hace parte de una de las más grandes del país, fueron declarados por la UNESCO como patrimonio inmaterial de la humanidad ya que hacen parte del pequeño grupo de pueblos que aún mantienen su tradición oral.";
+    var y = document.getElementById("texto11").innerHTML = "PUNTA GALLINAS";
+    var y = document.getElementById("texto12").innerHTML = "Atractivo único para el disfrute del paisaje y la tranquilidad. Es la punta más norte de Colombia. El mar caribe junto al desierto hacen de este destino un lugar único en su belleza.";
+    var y = document.getElementById("texto13").innerHTML = "MANAURE";
+    var y = document.getElementById("texto14").innerHTML = "Es uno de los lugares más importantes en la explotación de salinas. Las Salinas de Manaure es uno de los atractivos más reconocidos en la Guajira, sus visitantes hacen recorridos donde un guía especializado les explica todo el proceso de la explotación.";
+    var y = document.getElementById("texto15").innerHTML = "PALOMINO";
+    var y = document.getElementById("texto16").innerHTML = "Palomino no es solo una playa, es un lugar donde la música abunda, con gente alegre, amable y sonriente. Aquí la música te hace libre y feliz. Este lugar siempre recibe a sus visitantes con las puertas abiertas y un clima muy cálido, perfecto para unas maravillosas vacaciones.";
+    var y = document.getElementById("texto17").innerHTML = "SANTUARIO DE FAUNA Y FLORA LOS FLAMENCOS";
+    var y = document.getElementById("texto18").innerHTML = "Ubicado en el corregimiento de camarones, un lugar donde podrás disfrutar de uno de los espectáculos más hermosos, donde cientos de aves rosadas; flamencos pasean por el cielo.";
+    var y = document.getElementById("texto19").innerHTML = "• PLANES Y PRECIOS •";
+    var y = document.getElementById("texto19").style.color = "#E36E4C";
+    var y = document.getElementById("texto20").innerHTML = "Contamos con opciones para tu disponibilidad de tiempo y presupuesto";
+    var y = document.getElementById("texto20").style.color = "#E36E4C";
+    var y = document.getElementById("texto21").innerHTML = "PLAN";
+    var y = document.getElementById("texto22").innerHTML = "TOUR";
+    var y = document.getElementById("texto23").innerHTML = "PRECIO (POR PERSONA)";
+    var y = document.getElementById("texto24").innerHTML = "Punta gallinas";
+    var y = document.getElementById("texto25").innerHTML = "3 días - 2 noches";
+    var y = document.getElementById("texto26").innerHTML = "$790.000";
+    var y = document.getElementById("texto27").innerHTML = "Cabo de la vela";
+    var y = document.getElementById("texto28").innerHTML = "2 días - 1 noche";
+    var y = document.getElementById("texto29").innerHTML = "$390.000";
+    var y = document.getElementById("texto30").innerHTML = "Flamencos + Punta gallinas";
+    var y = document.getElementById("texto31").innerHTML = "4 días - 3 noches";
+    var y = document.getElementById("texto32").innerHTML = "$1.030.000";
+    var y = document.getElementById("texto33").innerHTML = "Palomino + flamencos + Punta gallinas";
+    var y = document.getElementById("texto34").innerHTML = "5 días - 4 noches";
+    var y = document.getElementById("texto35").innerHTML = "$1.330.000";
+    var y = document.getElementById("texto36").innerHTML = "Si estás pensando en tomar uno de nuestros planes, ten en cuenta nuestra checklist para que tengas todo lo necesario antes de iniciar tu aventura:";
+    var y = document.getElementById("texto37").innerHTML = "Llevar ropa para clima ¡muy cálido!";
+    var y = document.getElementById("texto38").innerHTML = "Bloqueador solar y repelente";
+    var y = document.getElementById("texto39").innerHTML = "Tenis cómodos para caminar";
+    var y = document.getElementById("texto40").innerHTML = "Ropa de baño (Siempre hay un lugar)";
+    var y = document.getElementById("texto41").innerHTML = "Mochila pequeña para estar cómodos en el desierto";
+    var y = document.getElementById("texto42").innerHTML = "¿Tienes lo necesario?";
+    var y = document.getElementById("texto42").style.color = "#E36E4C";
+    var y = document.getElementById("texto43").innerHTML = "Dejanos tus datos para enviarte más información y ponernos en contacto contigo!";
+    var y = document.getElementById("texto43").style.color = "#E36E4C";
+    var y = document.getElementById("texto44").innerHTML = "Nombre completo";
+    var y = document.getElementById("texto45").innerHTML = "Email";
+    var y = document.getElementById("texto46").innerHTML = "Celular";
+    var y = document.getElementById("texto47").innerHTML = "¿Qué plan te interesa?";
+    var y = document.getElementById("texto48").innerHTML = "Nuestros planes";
+    var y = document.getElementById("texto49").innerHTML = "Punta gallinas";
+    var y = document.getElementById("texto50").innerHTML = "Cabo de la vela";
+    var y = document.getElementById("texto51").innerHTML = "Flamencos + Punta gallinas";
+    var y = document.getElementById("texto52").innerHTML = "Palomino + flamencos + Punta gallinas";
+    var y = document.getElementById("texto53").innerHTML = "Acepto los términos y condiciones y el tratamiento de mis datos.";
+    var y = document.getElementById("texto54").innerHTML = "Quiero recibir informaciones en mi correo electronico sobre promociones y descuentos.";
+    var y = document.getElementById("texto55").innerHTML = "Página creada con HTML, Bootstrap 5 y CSS, 2022.";
+}
+
+function calculadora() {
+
+    var n1 = prompt("Primer número")
+    var n2 = prompt("Segundo número")
+    var op = prompt("¿Qué operación quieres realizar?, escribe una de las siguientes operciones: +, -, *, /")
+
+    if (op == "+")
+        alert("El resultado es: " + (Number(n1) + Number(n2)))
+
+    if (op == "-")
+        alert("El resultado es: " + (Number(n1) - Number(n2)))
+
+    if (op == "*")
+        alert("El resultado es: " + (Number(n1) * Number(n2)))
+
+    if (op == "/")
+        alert("El resultado es: " + (Number(n1) / Number(n2)))
+
+}
